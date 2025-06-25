48de086b1fb915e42b6826269f3564492051f540
emilly <emilly7343@gmail.com>
2025-06-25 22:59:30 +0900

    팔구현

---
+++ Assets/Assets/Script/HandController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField]
+    private Hand currentHand;   //현재장착된 Hand형 타입 무기
+
+    //공격 중??
+    private bool isAttack = false;
+    private bool isSwing = false;
+
+    private RaycastHit hitinfo;
+    
+    void Update()
+    {
+        TryAttack();
+    }
+
+    private void TryAttack()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            if(!isAttack)
+            {
+                StartCoroutine(AttackCoroutine());
+
+            }
+        }
+    }
+
+    IEnumerator AttackCoroutine()
+    {
+        isAttack = true;
+        currentHand.anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(currentHand.attackDelayA);
+        isSwing = true;
+
+        //공격활성화 시점
+
+        yield return new WaitForSeconds(currentHand.attackDelayB);
+        isSwing = false;
+
+        yield return new WaitForSeconds(currentHand.attackDelay - currentHand.attackDelayA - currentHand.attackDelayB);
+        isAttack = false;
+    }
+    IEnumerator HitCoroutine()
+    {
+        while(isSwing)
+        {
+            if(CheckObject())
+            {
+                isSwing = false;
+            }
+            yield return null;
+        }
+    }
+
+    private bool CheckObject()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitinfo, currentHand.range))
+        {
+            return true; 
+        }
+        
+        return false;
+    }
+    
+}
