21d4637864b24b3f33d5d9141009291ff22e08d6
emilly <emilly7343@gmail.com>
2025-07-13 00:09:40 +0900

    돼지사냥

--- Assets/Assets/Script/PickaxeController.cs
+++ Assets/Assets/Script/PickaxeController.cs
@@ -27,15 +27,22 @@ public class PickaxeController : CloseWeaponController
             if (CheckObject())
             {
                 if (hitInfo.transform.tag == "Rock")
-                {
                     hitInfo.transform.GetComponent<Rock>().Mining();
+                else if (hitInfo.transform.tag == "NPC")
+                {
+                    SoundManager.instance.PlaySE("Animal_Hit");
+                    hitInfo.transform.GetComponent<Pig>().Damage(1, transform.position);
                 }
+
+
+
                 isSwing = false;
                 Debug.Log(hitInfo.transform.name);
             }
             yield return null;
         }
     }
+
 
     public override void CloseWeaponChange(CloseWeapon _closeWeapon)
     {
